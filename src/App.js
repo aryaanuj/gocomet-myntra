01e9700e3644d27c6b8555272b797ddb898cc627
anuj <anujarya6121998@gmail.com>
2021-02-09 00:46:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/font-awesome/css/font-awesome.css";
+import Header from "./components/Header";
+import Network from './Network';
+import Details from './components/Details';
+import Modal from './components/Modal';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return(
+		<React.Fragment>
+			<Header />
+			<BrowserRouter>
+				<Switch>
+					<Route exact path="/" component={Network} />
+					<Route path="/details" component={Details} /> 
+				</Switch>
+			</BrowserRouter>
+			
+		</React.Fragment>
+	);
 }
-
 export default App;
